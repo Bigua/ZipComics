08891a25c2818bc471ffce6689db998caec3ae75
Biguá <bigua.kun@gmail.com>
2014-12-26 16:22:37 -0200

    organizando
    organizando o codigo

--- zipcomics.py
+++ zipcomics.py
@@ -1,26 +1,32 @@
 #!/usr/bin/env python
 from  __future__  import with_statement
 from contextlib import closing 
-import os
-import  imghdr
 from zipfile import ZipFile , ZIP_DEFLATED
+import  imghdr
+import os
 
-#pegando o caminho
+#caminho de onde o script esta rodando
 input_var = os.path.dirname(os.path.abspath(__file__))
-#ando pelas pastas
+
+#caminhar pelas pastas
 for root, dirs, files in os.walk(input_var):
-	filename = os.path.split(root)[1]+r".cbz"
+
+	#nome da pasta onde estao as imagens
+	folder_name = os.path.split(root)[1]+r".cbz"
 	
+	#teste para evitar zips vazios
 	has_imgs = False
 	for name in files:
 		page  =  os.path.join(root,name)
 		if (imghdr.what(page)) == "jpeg":
 			has_imgs = True
-
 	if  has_imgs:
-		with  closing ( ZipFile(filename, "w",  ZIP_DEFLATED) )as zipcomic:	
+
+		#criando o zip so com imagens
+		with  closing ( ZipFile(folder_name, "w",  ZIP_DEFLATED) ) as zipcomic:	
 			for name in files:
 				page  =  os.path.join(root,name)
 			 	if (imghdr.what(page)) == "jpeg":
-			 		zfn = page[len(input_var)+len(os.sep):]
-			 		zipcomic.write(page,zfn)
+			 		#cordando o path do arquivo
+			 		arc_name = page[len(input_var) + len(os.sep):]
+			 		zipcomic.write(page,arc_name)
