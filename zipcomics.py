1dd32f3d70af53da0978080b1c564c54b4d83239
Biguá <bigua.kun@gmail.com>
2014-12-28 22:09:16 -0200

    Separando em método
    Separando o código em métodos
    Config pra aceitar o caminho na chamada do script

--- zipcomics.py
+++ zipcomics.py
@@ -1,35 +1,67 @@
 #!/usr/bin/env python
+ # -*- coding: utf-8 -*-
 from  __future__  import with_statement
-from contextlib import closing 
-from zipfile import ZipFile , ZIP_DEFLATED
-import  imghdr
 import os
 
-#caminho de onde o script esta rodando
-input_var = os.path.dirname(os.path.abspath(__file__))
+def help():
+	import textwrap
+	print textwrap.dedent("""\
+	zipcomics  - script para compactação de imagens em cbz
+	Uso: python zipcomics /caminho/para/as/pastas/com/img
+	""")
 
-#caminhar pelas pastas
-for root, dirs, files in os.walk(input_var):
+# metodo principal  que compacta as imagens
+def  zip(base_dir):
 
-	#nome da pasta onde estao as imagens
-	folder_name = os.path.split(root)[1]+r".cbz"
-	
-	#teste para evitar zips vazios
-	has_imgs = False
-	for name in files:
-		page  =  os.path.join(root,name)
-		if (imghdr.what(page)) == "jpeg":
-			has_imgs = True
-	if  has_imgs:
-		print "criando : " + folder_name
-		#criando o zip so com imagens
-		with  closing ( ZipFile(folder_name, "w",  ZIP_DEFLATED) ) as zipcomic:	
-			for name in files:
-				page  =  os.path.join(root,name)
-			 	if (imghdr.what(page)) == "jpeg":
+	from contextlib import closing
+	from zipfile import ZipFile , ZIP_DEFLATED
+	import  imghdr
 
-			 		#cordando o path do arquivo
-			 		arc_name = page[len(input_var) + len(os.sep):]
-			 		zipcomic.write(page,arc_name)
-			 	else:
-			 		print "ignorado : " + name + " -  inconpativel com img"
+	#caminhar pelas pastas
+	for root, dirs, files in os.walk(base_dir):
+
+		#nome da pasta onde estão as imagens
+		folder_name = os.path.split(root)[1]+r".cbz"
+
+		#teste para evitar zips vazios
+		has_imgs = False
+		for name in files:
+			page  =  os.path.join(root,name)
+			if (imghdr.what(page)) == "jpeg":
+				has_imgs = True
+		if  has_imgs:
+			print "criando : " + folder_name
+			#criando o zip so com imagens
+			with  closing ( ZipFile(folder_name, "w",  ZIP_DEFLATED) ) as zipcomic:
+				for name in files:
+					page  =  os.path.join(root,name)
+					if (imghdr.what(page)) == "jpeg":
+
+						#cortando o path do arquivo
+						arc_name = page[len(base_dir) + len(os.sep):]
+						zipcomic.write(page,arc_name)
+					else:
+						print "ignorado : " + name + " -  incompatível com img"
+	move(base_dir)
+
+
+#metodo que move os arquivos para o diretório de origem das imagens
+def move(base_dir):
+	import shutil
+
+	#raiz de onde o script está
+	root = os.path.split(os.path.abspath(__file__))[0]
+	files = [f for f in os.listdir( root) if os.path.isfile(f)]
+	for f in files:
+		extension = os.path.splitext(f)[1]
+		if extension == ".cbz":
+			shutil.move( root+os.sep+f,base_dir+os.sep+f)
+			print "Salvo em " + base_dir+os.sep+f
+
+#tudo começa aqui
+if __name__ ==  '__main__' :
+	import sys
+	if len(sys.argv) < 2:
+		help()
+	else:
+		zip( sys.argv[1])
